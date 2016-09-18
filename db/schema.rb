e9a46f0e21b7df4dd26175291b217147a96d354a
jfernapa <jfernapa@gmail.co>
2016-09-18 20:51:08 +0200

    Migrate sqlite database to postgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160917074631) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.string   "status"
     t.integer  "zombie_id"
