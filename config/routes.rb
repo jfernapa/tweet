81ebd599f6ee741dff87dfa170edcb5b86645e1d
jfernapa <jfernapa@gmail.co>
2016-09-29 20:54:02 +0200

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,17 @@ Rails.application.routes.draw do
   root 'tweet#index'
 
   get 'tweet/show'
-  root 'tweet#show'
 
   get 'tweet/new'
-  root 'tweet#new'
 
   get 'tweet/edit'
-  root 'tweet#edit'
 
   get 'tweet/login'
-  root 'tweet#login'
 
   get 'tweet/create'
-  root 'tweet#create'
 
   get 'tweet/update'
-  root 'tweet#update'
 
   get 'tweet/delete'
-  root 'tweet#delete'
 
 end
